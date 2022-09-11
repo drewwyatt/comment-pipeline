036f510b88b3af5407c033eaab054d6eb1d0aaae
Drew Wyatt <drew.j.wyatt@gmail.com>
2022-09-11 18:00:00 -0400

    add better logging

--- src/main.ts
+++ src/main.ts
@@ -7,30 +7,35 @@ import parseOutputs from './parse-outpits'
 
 async function run(): Promise<void> {
   try {
-    core.debug(`checking comment for commands: ${inputs.commands}...`)
+    core.debug('Cogging inputs...')
+    core.debug(`inputs.commands: ${inputs.commands}`)
+    core.debug(`inputs.comment.body: ${inputs.comment.body}`)
+    core.debug(`inputs.comment.id: ${inputs.comment.id}`)
 
-    core.debug(`comment body: "${inputs.comment.body}"`)
+    core.info(`Checking comment for commands...`)
     const command = inputs.commands.find(c => inputs.comment.body.startsWith(c))
 
     if (!command) {
-      core.debug('no command found in comment body. exiting...')
+      core.info('No command found in comment body. Exiting. 👋')
       return
     }
 
-    core.debug(`found command: ${command}...`)
+    core.info(`Found command: "${command}"...`)
     outputs.setCommand(command)
 
     const { arguments: args, invocation } = parseOutputs(command, inputs.comment)
 
-    core.debug(`setting args to: ${args}`)
+    if (args) {
+      core.info(`Setting args to: "${args}"`)
+    }
     outputs.setArguments(args)
 
-    core.debug(`setting invocation to: ${invocation}`)
+    core.debug(`Setting invocation to: "${invocation}"`)
     outputs.setInvocation(invocation)
 
-    core.debug('creating octokit...')
+    core.debug('Creating octokit...')
     const octokit = github.getOctokit(inputs.token)
-    core.debug(`acknowledging comment with id ${inputs.comment.id}...`)
+    core.debug(`Acknowledging comment with id ${inputs.comment.id}...`)
     await octokit.rest.reactions.createForIssueComment({
       owner: github.context.repo.owner,
       repo: github.context.repo.repo,
@@ -38,7 +43,7 @@ async function run(): Promise<void> {
       content: inputs.reaction,
     })
 
-    core.debug('done!')
+    core.info('Done! 👋')
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
